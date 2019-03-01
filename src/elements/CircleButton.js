88868e5871d4f37bbd0fdfc813a521e7d84f98ff
Sato3 <sato3@taketin.com>
2019-03-01 22:34:04 +0900

    Style MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
     return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonTitle}>{this.props.children}</Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+          {this.props.children}
+        </Text>
       </View>
     );
   }
